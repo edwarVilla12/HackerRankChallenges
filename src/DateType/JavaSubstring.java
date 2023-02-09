f1c5ef7e604e9d31e0b77cbbec4a713c9ec1cde3
root <root@LAPTOP-BSI4L24V.localdomain>
2023-02-08 19:08:44 -0500

    Substring challenge

--- src/DateType/JavaSubstring.java
+++ src/DateType/JavaSubstring.java
@@ -2,24 +2,26 @@ package DateType;
 
 import java.util.Scanner;
 
-public class StdinStdout {
+public class JavaSubstring {
 
     public static void main(String[] args) {
         /*
             Enter your code here. Read input from STDIN.
             Print output to STDOUT. Your class should be named Solution.
         */
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Type the a value: ");
-        int a = sc.nextInt();
-        System.out.println("Type the b value: ");
-        int b = sc.nextInt();
-        System.out.println("Type the c value: ");
-        int c = sc.nextInt();
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("a: " + a +".32d");
-        System.out.println("b: " + b);
-        System.out.println("c: " + c);
+        String s = scanner.nextLine();
+        StringBuilder stringBuilder = new StringBuilder();
+        int start = scanner.nextInt();
+        int end  = scanner.nextInt();
+
+        for(int i = start; i < end; i++){
+            stringBuilder.append( s.substring(i , i + 1));
+        }
+
+        System.out.println(stringBuilder.toString());
+
     }
 
 }
